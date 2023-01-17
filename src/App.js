179778b45751e16bca3e39e12a0a99c6fcfb1c5f
Kevin Wu <kevin@kevinniuwu.com>
2023-01-17 17:47:12 -0500

    Fix build warnings

--- src/App.js
+++ src/App.js
@@ -7,36 +7,10 @@ const MQTT_TOPIC = "muddescapes-esp/test";
 
 function App() {
   const { client } = useMqttState();
-  const { message, connectionStatus } = useSubscription(MQTT_TOPIC);
+  const { message } = useSubscription(MQTT_TOPIC);
 
   // dummy state variable to get the component to rerender every second
   const [, setCurrTime] = React.useState(Date.now());
-  // console.log(message, connectionStatus);
-
-  // message will be defined when it's receiving a message through MQTT
-  React.useEffect(() => {
-    if (message) {
-      const messageStr = message.message;
-      console.log(messageStr);
-      unpackMQTT(messageStr);
-    }
-  }, [message]);
-
-  React.useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrTime(Date.now());
-    }, 1000);
-
-    return () => clearInterval(interval);
-  });
-
-  const reference = {
-    var1: { options: ["true", "false"], realState: "false" },
-    var2: { options: ["red", "blue"], realState: "red" },
-    var3: { options: ["open", "closed"], realState: "open" },
-  };
-
-  const MQTTmessage = "door-status {open, closed, halfway} status=closed";
 
   const [vars, setVars] = React.useState({});
   const [changedVars, setChangedVars] = React.useState([]);
@@ -50,9 +24,69 @@ function App() {
   );
 
   // updating the current state of one of the variables (name) to value
-  function setTempStateHelper(name, value) {
-    setTempState({ ...tempState, [name]: value });
-  }
+  const setTempStateHelper = React.useCallback(
+    (name, value) => {
+      setTempState({ ...tempState, [name]: value });
+    },
+    [tempState]
+  );
+
+  const unpackMQTT = React.useCallback(
+    (message) => {
+      if (message === "refresh") {
+        return;
+      }
+
+      let varName = message.split(" ")[0];
+
+      if (varName === "set") {
+        return;
+      }
+
+      let options = message
+        .split("{")[1]
+        .split("}")[0]
+        .replace(/ /g, "")
+        .split(",");
+      let currState = message.split("status=").at(-1);
+
+      if (changedVars.includes(varName)) {
+        return;
+      }
+
+      setVars({
+        ...vars,
+        [varName]: {
+          options: options,
+          realState: currState,
+          changedAt: Date.now(),
+        },
+      });
+
+      setTempStateHelper(varName, currState);
+    },
+    [vars, setVars, setTempStateHelper, changedVars]
+  );
+
+  // message will be defined when it's receiving a message through MQTT
+  React.useEffect(() => {
+    if (message) {
+      const messageStr = message.message;
+      console.log(messageStr);
+      unpackMQTT(messageStr);
+    }
+    // TODO: if we add the unpackMQTT function to the dependency array, it will cause an infinite loop
+    // just disable the warning for now
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [message]);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrTime(Date.now());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  });
 
   return (
     <div className="App">
@@ -103,36 +137,6 @@ function App() {
     </div>
   );
 
-  function unpackMQTT(message) {
-    let varName = message.split(" ")[0];
-
-    if (varName === "set") {
-      return;
-    }
-
-    let options = message
-      .split("{")[1]
-      .split("}")[0]
-      .replace(/ /g, "")
-      .split(",");
-    let currState = message.split("status=").at(-1);
-
-    if (changedVars.includes(varName)) {
-      return;
-    }
-
-    setVars({
-      ...vars,
-      [varName]: {
-        options: options,
-        realState: currState,
-        changedAt: Date.now(),
-      },
-    });
-
-    setTempStateHelper(varName, currState);
-  }
-
   // send message of mqtt with modified entries of currState
   function sayHello() {
     const changedNames = Object.entries(tempState)
@@ -152,16 +156,14 @@ function App() {
         tempState[changedNames[i]];
 
       console.log(newMessage);
-      client.publish(MQTT_TOPIC, newMessage);
+      client.publish(MQTT_TOPIC, newMessage, { qos: 2 });
     }
 
     setChangedVars([]);
   }
 
-  // delete this later
   function refresh() {
-    unpackMQTT(MQTTmessage);
-    console.log(tempState);
+    client.publish(MQTT_TOPIC, "refresh", { qos: 2 });
   }
 }
 
