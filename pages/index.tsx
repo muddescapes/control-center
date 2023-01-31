98f9776fc758472c8dd50a827827f7b5837ff9a7
Kevin Wu <kevin@kevinniuwu.com>
2023-01-31 01:03:30 -0500

    Add header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
-import Link from "next/link";
+import Image from "next/image";
+import logo from "../images/muddescapes-logo.png";
 
 export default function Home() {
   return (
-    <div>
-      Hello World. <Link href="/about">About</Link>
-    </div>
+    <>
+      <div className="flex flex-row items-center outline">
+        <Image src={logo} alt="MuddEscapes Logo" height={60} />
+        <span className="ml-4 text-4xl font-semibold">
+          MuddEscapes Control Center
+        </span>
+      </div>
+    </>
   );
 }
